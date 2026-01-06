c15c51b7ff33250deeb6091a9774e06593be1efa
“cristopher” <curibe26@aoiths.org>
2026-01-06 13:38:24 -0500

    update

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+// ==============================
+// Profile 1: Existing Object
+// ==============================
 var theranos = {
-	first: "Elizabeth",
-	last: "Holmes",
-	year: 1984,
-	college: [
-		"St. John University", "Stanford University"
-	],
-	img: "https://startupi.com.br/wp-content/uploads/2023/08/20160711elizabeth-holmes.jpg",
-    wiki: "https://en.wikipedia.org/wiki/Elizabeth_Holmes"
+  first: "Elizabeth",
+  last: "Holmes",
+  age: 41,
+  year: 1984,
+  college: ["St. John University", "Stanford University"],
+  img: "https://startupi.com.br/wp-content/uploads/2023/08/20160711elizabeth-holmes.jpg",
+  wiki: "https://en.wikipedia.org/wiki/Elizabeth_Holmes"
 };
+
+// ==============================
+// Profile 2: New Object (Elon Musk)
+// ==============================
+var elonMusk = {
+  first: "Elon",
+  middle: "Reeve",
+  last: "Musk",
+  age: 48,
+  college: [
+    "Queens College",
+    "Stanford University",
+    "Wharton School of the University of Pennsylvania"
+  ],
+  img: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg",
+  wiki: "https://en.wikipedia.org/wiki/Elon_Musk"
+};
+
